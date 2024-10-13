6c5a5718f08dba02773e4a82b0c0dd4db6d29ef3
ccprojectsjonell <georgebeard839@gmail.com>
2024-10-13 17:37:41 +0800

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -7,13 +7,14 @@ module.exports = {
   author: 'Jonell Magallanes',
   cooldowns: 10,
 
-  onCmds: async function ({ bot, args, event}) {
+  onCmds: async function ({ bot, args, event }) {
     const askQuery = args.join(' ') || 'Hello';
 
     try {
-      const response = await axios.get(`https://jonellprojectccapisexplorer.onrender.com/api/gptconvo?ask=${askQuery}&id=${event.sender.id}`);
-      const bold = global.fonts.bold("CHATGPT")
+      const response = await axios.get(`https://ccprojectsjonellapis-production.up.railway.app/api/gpt4o?ask=${askQuery}&id=${event.sender.id}`);
+      const bold = global.fonts.bold("CHATGPT");
       const apiResponse = response.data.response;
+
       bot.send(`${bold}\n${global.line}\n${apiResponse}`);
     } catch (error) {
       bot.send(error.message);
